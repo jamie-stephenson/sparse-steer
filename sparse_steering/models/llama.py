b613d6a735386ee246c3b6cf2f0eed2f1c2a80cb
jamie-stephenson <jamie.t.stephenson@gmail.com>
2026-02-17 19:02:32 +0000

    Initial commit

---
+++ sparse_steering/models/llama.py
@@ -0,0 +1,10 @@
+from transformers.models.llama.modeling_llama import (
+    LlamaMLP, 
+    LlamaAttention,
+)
+
+class MLP(LlamaMLP):
+
+class Attention(LlamaAttention):
+
+# maybe model wrapper for loading??
